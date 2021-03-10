5f05de9a405e144f64e22a19cc4ffe11220e77f2
Shubham Shandilya <73842314+shubhamsh14@users.noreply.github.com>
2021-03-10 20:12:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from fileHandling import JsonHandle
+from updateBMIProfile import UpdateBMIProfile
+from analysisBMI import AnalysisBMI
+
+class BMICalculator():
+    def __init__(self):
+        pass
+
+    jsonHandle = JsonHandle()
+    updateProfileData = UpdateBMIProfile()
+    analysisBMIData = AnalysisBMI()  
+    # Read Input Json File
+    defaultData = jsonHandle.readFile('InputData.json')
+    
+    # Update data according to task 1.
+    updatedData = updateProfileData.updateBMIProfile(defaultData)
+    
+    # Overweight count according to task 2.
+    overweightCount = analysisBMIData.countOverweight(updatedData)
+    print('Overweight Count : %d' %overweightCount)
+
+    # Write Updated data to output json file
+    jsonHandle.writeFile( defaultData,'OutputData.json')
+
+
+startBMICalculator = BMICalculator()
